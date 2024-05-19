e1fc1649b63fe8a8546ac708719b3b8261c6200a
Mohammad Aman <mohammadamaan1105@gmail.com>
2024-05-19 09:40:24 +0545

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import { dcrypt } from "./lib/session";
+import { SessionPayload } from "./lib/dal";
+
+// specifiy the routes
+const publicRoutes = ["/register", "/login"];
+const protectedRoutes = ["/dashboard"];
+
+export default async function middleware(request: NextRequest) {
+  // check the current user routes
+  const path = request.nextUrl.pathname;
+  const isPublicRoute = publicRoutes.includes(path);
+  const isProtectedRoute = protectedRoutes.includes(path);
+
+  // dcrypt session from the cookie
+  const session = request.cookies.get("session")?.value;
+  const payload = (await dcrypt(session!)) as SessionPayload;
+
+  // redirect user to login if not authenticated
+  if (isProtectedRoute && !payload?.userId) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  // redirect user to dashboard if authenticated
+  if (
+    isPublicRoute &&
+    payload?.userId &&
+    !request.nextUrl.pathname.startsWith("/dashboard")
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+}
+// Routes Middleware should not run on
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
